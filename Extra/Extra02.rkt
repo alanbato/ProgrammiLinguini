54d86a82d7ce2b396db6b118c16bdf5f5b3a8ff2
Alan Velasco <AlanVelasco.A@gmail.com>
2018-02-06 10:58:03 -0600

    Add extra excercise 02

---
+++ Extra/Extra02.rkt
@@ -0,0 +1,30 @@
+(define (prime? x)
+  (display "Not Implemented")
+)
+
+(define (sum-digits x)
+  (display "Not Implemented")
+)
+
+(define (advanced-mean x)
+  (display "Not Implemented")
+)
+
+(define (curried-do-smt x)
+  (display "Not Implemented")
+)
+
+(define (merge x)
+  (display "Not Implemented")
+)
+
+(define (list->number x)
+  (display "Not Implemented")
+)
+
+(define (complete-graphs x)
+  (display "Not Implemented")
+)
+(define (sum-matrix x)
+  (display "Not Implemented")
+)
