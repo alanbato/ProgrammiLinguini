ad10351ab53c58c85ec118d43e41b5092904c63d
Alan Velasco <AlanVelasco.A@gmail.com>
2018-02-06 20:03:39 -0600

    Add comments

--- Extra/Extra02.rkt
+++ Extra/Extra02.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;;; (by (and '("Alan" "Velasco" "A01113373") '("Enrique" "Barragan" "A01370878")))
+
+;;; Prime Numbers
 (define (prime-helper x y)
   (cond
     [(= y 1) #t]
@@ -15,10 +18,12 @@
   )
 )
 
+;;; Count of digits
 (define (count-digits l)
   (apply + (map string-length (map number->string l)))
 )
 
+;;; Advanced Mean
 (define (advanced-mean-helper x l)
     (cond
       [(zero? x)
@@ -37,10 +42,12 @@
   (advanced-mean-helper (read) l)
 )
 
+;;; Currying
 (define (curried-do-smt x)
   (lambda (y z) (- (+ (* x y) z) 1 ))
 )
 
+;;; Merging Two Sorted Lists
 (define (merge x y)
   (cond
    [(null? x) y]
@@ -54,20 +61,22 @@
   )
 )
 
+;;; Boolean Operations
 (define (to_bool x) (if (equal? x 1) #t #f))
 (define (to_int x) (if x 1 0))
 (define (boolean_ops x y)
   (map (lambda (a b) (to_int (or (to_bool a) (to_bool b)))) x y)
 )
 
+;;; List to Number
 (define (list->number x)
   (string->number (apply string-append (map number->string x)))
 )
 
+;;; Complete Graphs
 (define (in-all-edges? x y)
-;;; 1 '((1 2 3) (1 2)) -> #t
-  (cond ;;; An OR could be here, but we want to return a bool so we use cond
-    [(null? y) #t] ;;; Last call is not important
+  (cond ; An OR could be here, but we want to return a bool so we use cond
+    [(null? y) #t] ; Last call is not important
     [(and (member x (car y)) (in-all-edges? x (cdr y))) #t]
     [else #f]
   )
@@ -77,13 +86,14 @@
   (cond 
     [(null? x) #f]
     [(null? (cdr x)) #f]
-    [(null? (car x)) #t] ;;; Last call is not important
+    [(null? (car x)) #t] ; Last call is not important
     [else (and
             (in-all-edges? (caar x) (cdr x))
             (complete-graphs (cons (cdar x) (cdr x))))]
   )
 )
 
+;;; Sum of matrix elements
 (define (sum-matrix-helper x accum)
   (if (null? x)
     accum
