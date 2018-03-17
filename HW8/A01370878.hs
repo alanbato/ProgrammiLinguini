5090392833cc322d57f84a03c1ecea93e425488a
Enrique Barragan <ebarragan1997@gmail.com>
2018-03-17 12:51:22 -0600

    hw8

--- HW8/A01370878.hs
+++ HW8/A01370878.hs
@@ -1,11 +1,41 @@
 module HW8 where
-  import Data.List
 
-  mmult :: Num a => [[a]] -> [[a]] -> [[a]] 
-  mmult a b = [ [ sum $ zipWith (*) ar bc | bc <- (transpose b) ] | ar <- a ]
-  
-  mean :: (Fractional a) => [a] -> a
-  mean [] = 0
-  mean xs = sum xs / Data.List.genericLength xs
+    transpose :: [[Int]] -> [[Int]]
+    transpose ([]:xs) = []
+    transpose mat = (map head mat) : (transpose(map tail mat))
 
-  avgh list = [("Male", mean (map (\(gender:age) -> age) (filter (\(gender:height) -> gender == "Male") list))), ("Female", mean (filter (\(gender:height) -> gender == "Female") list))]
+    rowMat :: [Int] -> [[Int]] -> [Int]
+    rowMat x [] = []
+    rowMat x (y:ys) = sum (map (\(x,y) -> x * y) (zip x y)) : rowMat x ys
+
+    matMul :: [[Int]] -> [[Int]] -> [[Int]]
+    matMul ([]:xs) y = []
+    matMul x y = map (\x -> rowMat x (transpose y)) x
+    
+    meanHeight :: [(String, Float)] -> [(String, Float)]
+    meanHeight tbl = [(fst (head tbl), ((sum (map snd tbl)) / fromIntegral(length tbl)))]
+
+    -- [("Male", 175), ("Female",150), ...]
+    summarize :: [(String, Float)] -> [(String, Float)]
+    summarize tbl = (meanHeight (filter (\(x,y) -> x == "Male") tbl)) ++
+                    (meanHeight (filter (\(x,y) -> x == "Female") tbl))
+
+    smallerThan :: [(String, Float)] -> Float -> [(String, Float)]
+    smallerThan [] n = []
+    smallerThan (x:xs) n
+                    | (snd x) < n = x : (smallerThan xs n)
+                    | otherwise = (smallerThan xs n)
+    
+    data Node = Node Int | String deriving (Show)
+    data DGraph = DGraph [(Node, [Node])] deriving (Show)
+    maxDegree :: DGraph -> (Node, Int)
+    maxDegree (DGraph []) = error "maxDegree on empty graph is not defined"
+    maxDegree (DGraph [(node, edges)]) = (node, length edges)
+    maxDegree (DGraph ((node, edges):xs))
+                        | (length edges) >= (snd other) = (node, length edges)
+                        | otherwise = other
+                        where other = maxDegree (DGraph xs)
+
+
+    pathExists :: Node -> Node -> DGraph -> Bool
+    pathExists n1 n2 dgph = error "Error"
