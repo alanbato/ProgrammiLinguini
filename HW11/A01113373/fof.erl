82551d158b9b43bb61316e2ff68869c4dadfcf2c
Alan Velasco <AlanVelasco.A@gmail.com>
2018-04-19 22:48:58 -0500

    Finished my fof

--- HW11/A01113373/fof.erl
+++ HW11/A01113373/fof.erl
@@ -3,8 +3,15 @@
 
 listen(Color) -> 
   receive
-    {PID, M} -> ;
-    {PID, C, M} when C = Color -> io:format("Received a message from a friend process, ~p: '~p'.~n". [PID, M])
+    {PName, M} -> PName ! {self(), identify, M},
+                  listen(Color);
+    {PID, identify, M} -> PID ! {self(), Color, M},
+                listen(Color);
+    {PID, C, M} when C == Color -> io:format("Received a message from a friend process, ~p: '~p'.~n", [PID, M]),
+                                  listen(Color);
+    {PID, C, M} when C /= Color -> io:format("Process ~p is not my friend. I will ignore its message.~n", [PID]),
+                                  listen(Color)
+  end.
 
 start(Id, Color) ->
   register(Id, spawn(fof, listen, [Color])).
